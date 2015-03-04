cb70e490809eaf0a29591d29123079d7d5e24664
nagachaitanya <spikynaga@gmail.com>
2015-03-04 01:12:57 -0800

    minor chages

--- test.ps1
+++ test.ps1
@@ -2,6 +2,7 @@ Param(
     [Parameter(Mandatory=$True)]
     $Directory
 )
+{
 write-host "Creating a directory.."
-New-Item -ItemType directory -Path C:\$(get-date -f yyyy-MM-dd-hh-mm-ss)-$Directory
-
+New-Item -ItemType directory -Path 'C:\$(get-date -f yyyy-MM-dd-hh-mm-ss)-$Directory'
+}
