a1986326ed099bbf0003751ede7fc3df7a15b87f
nagachaitanya <spikynaga@gmail.com>
2015-03-09 00:27:02 -0700

    minor changes

--- test.ps1
+++ test.ps1
@@ -4,5 +4,5 @@ Param(
     $Directory
 )
 write-host "Creating a directory.."
-New-Item -ItemType directory -Path C:\$(get-date -f yyyy-MM-dd-hh-mm-ss)-$Directory
+New-Item -ItemType directory -Path C:\$(get-date -f yyyy-MM-dd-hh-mm-ss)-$($env:Directory)
 
